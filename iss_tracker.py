efdd06af61088512c782bc4c55268b91afaef3e9
Ubuntu <ubuntu@pa8729-vm.js2local>
2023-03-07 05:03:28 +0000

    started location route

--- iss_tracker.py
+++ iss_tracker.py
@@ -3,7 +3,8 @@
 from flask import Flask, request
 import requests
 import xmltodict
-from math import sqrt
+import math
+from geopy.geocoders import Nominatim
 
 r = requests.get('https://nasa-public-data.s3.amazonaws.com/iss-coords/current/ISS_OEM/ISS.OEM_J2K_EPH.xml')
 iss_data = xmltodict.parse(r.text)
@@ -67,12 +68,12 @@ def vectors(epoch: str) -> list:
         state_vectors (list): state vectors of the specified EPOCH
     """
     data = data_set()
+    state_vectors = []
     try:
         for i in range(len(data['ndm']['oem']['body']['segment']['data']['stateVector'])):
             if (data['ndm']['oem']['body']['segment']['data']['stateVector'][i]['EPOCH'] == epoch):
-                state_vectors = []
                 state_vectors.append(data['ndm']['oem']['body']['segment']['data']['stateVector'][i])
-            return state_vectors
+                return state_vectors
     except KeyError:
         return "Data not loaded in\n"
     
@@ -92,9 +93,35 @@ def epoch_speed(epoch: str) -> dict:
         sumSpeedSquare = pow(float(data[0]['X_DOT']['#text']), 2) + pow(float(data[0]['Y_DOT']['#text']), 2) + pow(float(data[0]['Z_DOT']['#text']), 2)
     except TypeError:
         return "Data not loaded in\n"
-    speed['Speed of EPOCH'] = (sqrt(sumSpeedSquare)) #magnitude of speed utilizing the x, y, and z components of speed
+    speed['Speed of EPOCH'] = (math.sqrt(sumSpeedSquare)) #magnitude of speed utilizing the x, y, and z components of speed
     return speed
 
+@app.route('/epochs/<epoch>/location', methods = ['GET'])
+def epoch_location(epoch: str) -> dict:
+    data = vectors(epoch)
+    mean_earth_radius = 6378.137 #earth radius in km
+    x = float(data[0]['X']['#text'])
+    y = float(data[0]['Y']['#text'])
+    z = float(data[0]['Z']['#text'])
+    hrs = float(epoch[9:10])
+    mins = float(epoch[12:13])
+    lat = math.degrees(math.atan2(z, math.sqrt(pow(x, 2)+pow(y, 2))))
+    lon = math.degrees(math.atan2(y, x)) - ((hrs-12)+(mins/60))*(360/24) + 24
+    if lon > 180: #handling cases where longitude outside of earth latitude range -180 => 180 degrees
+        lon -= 360
+    elif lon < -180:
+        lon += 360 
+    alt = math.sqrt(pow(x, 2)+pow(y, 2)+pow(z, 2)) - mean_earth_radius
+    geocoder = Nominatim(user_agent='iss_tracker')
+    geolocation = geocoder.reverse((lat, lon), zoom=15, language='en')
+    location_data = {'latitude': lat, 'longitude': lon, 'altitude': alt, 'geolocation': str(geolocation)}
+    #location_data = {}
+    #location_data['latitude'] = lat
+    #location_data['longitude'] = lon
+    #location_data['altitude'] = alt
+    #location_data.update(geolocation)
+    return location_data
+    
 #to run: curl -X DELETE localhost:5000/delete-data
 @app.route('/delete-data', methods = ['DELETE'])
 def del_data():
@@ -113,16 +140,29 @@ def retrieve_data():
 @app.route('/comment', methods = ['GET'])
 def display_comments():
     data = data_set()
-    comments = []
-    for i in range(len(data['ndm']['oem']['body']['segment']['data']['COMMENT'])):
-        comments.append(data['ndm']['oem']['body']['segment']['data']['COMMENT'][i])
-    #for ndm, oem, body, segment, datas in data.items():
+    return data['ndm']['oem']['body']['segment']['data']['COMMENT']
+    #for i in range(len(data['ndm']['oem']['body']['segment']['data']['COMMENT'])):
+     #   comments.append(data['ndm']['oem']['body']['segment']['data']['COMMENT'][i])
+    #for ndm, oem, body, segment, datas, comment in data.items():
         #for val in datas:
             #comments.append(datas[val])        
     #res = [val[temp] for key, val in data.items() if temp in val]
     #print(str(res))
-    return comments
+    #return comments
 
+@app.route('/header', methods = ['GET'])
+def display_header():
+    data = data_set()
+    return data['ndm']['oem']['header']
+    #for i in range(len(data['ndm']['oem']['header'])):
+     #   header.append(data['ndm']['oem']['header'][i])
+    #return header
+
+@app.route('/metadata', methods = ['GET'])
+def display_metadata():
+    data = data_set()
+    return data['ndm']['oem']['body']['segment']['metadata']
+    
 @app.route('/help', methods = ['GET'])
 def define_routes():
     return '''\nUsage: curl 'localhost:5000[OPTIONS]'\n
